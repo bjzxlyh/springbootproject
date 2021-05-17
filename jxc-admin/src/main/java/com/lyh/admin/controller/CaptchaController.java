05c9fa807b4c8062d6b8dca3ba37eeaabcff057f
liyiheng <834805011@qq.com>
2021-05-17 21:03:22 +0800

    验证码

---
+++ jxc-admin/src/main/java/com/lyh/admin/controller/CaptchaController.java
@@ -0,0 +1,27 @@
+package com.lyh.admin.controller;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@RestController
+public class CaptchaController {
+
+    @Resource
+    public DefaultKaptcha defaultKaptcha;
+
+    @RequestMapping(value="/image",method = RequestMethod.GET)
+    public void kaptcha(HttpSession session, HttpServletResponse response) throws IOException {
+        response.setDateHeader("Expires", 0);
+        response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+        response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+        response.setHeader("Pragma", "no-cache");
+        response.setContentType("image/jpeg");
+    }
+}
